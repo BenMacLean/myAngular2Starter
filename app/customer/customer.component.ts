00e71610d7644ab52e4472c16248dec25a2cdbbe
BenMacLean <benhmaclean@gmail.com>
2016-08-26 14:24:45 -0300

    list items component working

--- app/customer/customer.component.ts
+++ app/customer/customer.component.ts
@@ -1,10 +1,13 @@
-import {Component} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
+
 
 @Component({
     selector: 'app-customer',
-    templateUrl: 'app/customer/customer.component.html'
+    templateUrl: 'app/customer/customer.component.html',
+    directives: [CustomerComponent]
 })
-export class AppComponent {
+export class CustomerComponent implements OnInit {
+  @Input() customer: {id: number, name: string};
   constructor(){ }
 
     ngOnInit() { }
