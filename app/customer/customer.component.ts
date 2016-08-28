dbd3075938aa35fd434f9b5fe14b862517be467e
BenMacLean <benhmaclean@gmail.com>
2016-08-28 20:18:44 -0300

    "sunday"

--- app/customer/customer.component.ts
+++ app/customer/customer.component.ts
@@ -4,7 +4,6 @@ import {Component, Input, OnInit} from '@angular/core'
 @Component({
     selector: 'app-customer',
     templateUrl: 'app/customer/customer.component.html',
-    directives: [CustomerComponent]
 })
 export class CustomerComponent implements OnInit {
   @Input() customer: {id: number, name: string};
