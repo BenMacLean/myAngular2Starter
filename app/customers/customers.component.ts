5a84499e3b91668ae4eec55cd780811fbdc6b995
BenMacLean <benhmaclean@gmail.com>
2016-08-29 14:46:09 -0300

    setting up customers

---
+++ app/customers/customers.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: 'app/customers/customers.component.html'
+})
+export class CustomersComponent imlements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
