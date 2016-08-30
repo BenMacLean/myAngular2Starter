7ea3e9f5ff444f5504d56193ed48daaa44ec5c8b
BenMacLean <benhmaclean@gmail.com>
2016-08-30 10:16:40 -0300

    fixing ./

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -2,20 +2,23 @@ import {Component, Input, OnInit} from '@angular/core'
 
 import {CustomerComponent} from './customer.component';
 
+import {CustomerService} from './customer.service';
+
 @Component({
   moduleId: module.id,
   selector: 'app-customers',
   templateUrl: 'customers.component.html',
-  directives: [CustomerComponent]
+  directives: [CustomerComponent],
+  providers: [CustomerService]
 
 })
 export class CustomersComponent implements OnInit {
 
   customers: any[];
 
-  constructor() {}
+  constructor(private _customerService: CustomerService) {}
 
   ngOnInit() {
-    this.customers = [];
+    this.customers = this._customerService.getCustomers();
   }
 }
